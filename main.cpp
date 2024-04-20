02a7a3f3dc8fa3bc35a77590ea9352c88bab54f1
Kjakuz <kuba.kozicki7@gmail.com>
2024-04-20 14:33:58 +0200

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+
+using namespace std;
+
+void help(){
+    cout << "Help           Show this message" << endl;
+    cout << "Print          Print the tree usin In-order, Pre-order, Post-order" << endl;
+    cout << "Delete         Delete elements of the tree" << endl;
+    cout << "Delete ALL     Delete all elements of the tree" << endl;
+    cout << "Rebalance      Rebalance the tree" << endl;
+    cout << "FindMinMax     Find the largest and smallest element" << endl;
+    cout << "Exit           Exits the program" << endl;
+}
+
+
+
+int main(int argc, char* argv[]) {
+
+    int nodes;
+    int elements[nodes];
+
+
+
+    if (string(argv[1]) != "--tree" || (string(argv[2]) != "AVL" && string(argv[2]) != "BST")){
+        cout << "--tree [type of tree: BST/AVL]" << endl;
+    }
+
+    if (argc < 4){
+        cout << "nodes> ";
+        cin >> nodes;
+        cout << "insert> ";
+        for (int i = 0; i < nodes; ++i) {
+            cin >> elements[i];
+        }
+    }
+
+   while(true){
+        string action = "";
+        cout << "action> ";
+        cin >> action;
+
+        if (action == "Help"){
+            help();
+        }
+        else if (action == "Exit"){
+            exit(1);
+        }
+        else{
+            cout << "Wrong action! Type 'Help'" << endl;
+        }
+   }
+
+
+
+}
